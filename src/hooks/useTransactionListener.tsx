49f691209803aa0621838696261f9eecec7be599
Callum McIntyre <mcintyre1994@gmail.com>
2022-12-21 18:10:03 +0000

    Fix lint error

--- src/hooks/useTransactionListener.tsx
+++ src/hooks/useTransactionListener.tsx
@@ -5,29 +5,29 @@ import { useConnection } from "@solana/wallet-adapter-react";
 import { notify } from "utils/notifications";
 
 export default function useTransactionListener(reference: PublicKey) {
-    const { connection } = useConnection();
+  const { connection } = useConnection();
 
-    const mostRecentNotifiedTransaction = useRef<string | undefined>(undefined);
+  const mostRecentNotifiedTransaction = useRef<string | undefined>(undefined);
 
-    useEffect(() => {
-        const interval = setInterval(async () => {
-            try {
-                // Check if there is any transaction for the reference
-                const signatureInfo = await findReference(connection, reference, { until: mostRecentNotifiedTransaction.current });
+  useEffect(() => {
+    const interval = setInterval(async () => {
+      try {
+        // Check if there is any transaction for the reference
+        const signatureInfo = await findReference(connection, reference, { until: mostRecentNotifiedTransaction.current });
 
-                console.log('Transaction confirmed', signatureInfo);
-                notify({ type: 'success', message: 'Transaction confirmed', txid: signatureInfo.signature });
-                mostRecentNotifiedTransaction.current = signatureInfo.signature;
-            } catch (e) {
-                if (e instanceof FindReferenceError) {
-                    // No transaction found yet, ignore this error
-                    return;
-                }
-                console.error('Unknown error', e)
-            }
-        }, 500)
-        return () => {
-            clearInterval(interval)
+        console.log('Transaction confirmed', signatureInfo);
+        notify({ type: 'success', message: 'Transaction confirmed', txid: signatureInfo.signature });
+        mostRecentNotifiedTransaction.current = signatureInfo.signature;
+      } catch (e) {
+        if (e instanceof FindReferenceError) {
+          // No transaction found yet, ignore this error
+          return;
         }
-    }, [])
+        console.error('Unknown error', e)
+      }
+    }, 500)
+    return () => {
+      clearInterval(interval)
+    }
+  }, [connection, reference])
 }
